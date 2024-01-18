283d50827bbad33ffa6fd189a95e20eb375c3146
Tatekeck <tatekeckcomputer@gmail.com>
2024-01-18 07:56:25 -0500

    starting code

---
+++ sandbox.js
@@ -0,0 +1,26 @@
+//programer: Tate, k
+//Date : 1.20.2024
+//Program Sphero Bolt Testing
+
+async function startprogram() {
+	await speak ("hello World", true);
+	setMainled({r: 0, g: 0, b: 225});
+	setspeed(60);
+	await delay (2);
+	setSpeed(0)
+}
+*/
+
+
+// square w/ different colors for each line
+
+
+
+
+// square w/ different colors and sounds each line 
+
+
+
+
+
+// Figure 8
