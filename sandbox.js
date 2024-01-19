7fe33cd746d4ca86d187ddadaa1eb7b97c777e25
Tatekeck <tatekeckcomputer@gmail.com>
2024-01-19 07:37:55 -0500

    1.18.24

--- sandbox.js
+++ sandbox.js
@@ -1,26 +1,36 @@
-//programer: Tate, k
-//Date : 1.20.2024
-//Program Sphero Bolt Testing
+//Programer:Ryan the Jerzyk
+//Date: 1.10.2024
+//Program: Sphero Bolt Testing
+//Bolt ID: CF6D
 
-async function startprogram() {
-	await speak ("hello World", true);
-	setMainled({r: 0, g: 0, b: 225});
-	setspeed(60);
+async function startProgram() {
+	await speak("Hello World", true);
+	setMainLed({r:0, g: 0, b: 255 });
+	setSpeed(60);
 	await delay (2);
-	setSpeed(0)
+	setSpeed(0);
 }
-*/
 
 
-// square w/ different colors for each line
+//Square w/ diffrent colors for each line
+async function startprogram(){
+for (var _i1 = 0; _i1 < 4; _i1++) 
+	setMainLed(getRandomColor())
+	await delay(1)
+	await spin (90, 2)
+	
+
+}
 
 
 
 
-// square w/ different colors and sounds each line 
+
+
+//Square w/ diffrent colors and sound for each line
 
 
 
 
 
-// Figure 8
+//Figure 8 
