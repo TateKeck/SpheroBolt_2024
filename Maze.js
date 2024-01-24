cf5775a55e742bde87cc33b3892702502ea8dee3
Tatekeck <tatekeckcomputer@gmail.com>
2024-01-24 08:16:48 -0500

    blue check point

--- Maze.js
+++ Maze.js
@@ -8,5 +8,12 @@
 async function startProgram() {
 	await speak("START", true);
 	await scrollMatrixText('start', {r: 50, g: 50, b: 50}, 30, true)
+
+	
+//blue checpoint
+	await delay(1)
+	await roll(0,130,1)
+	await delay (1)
+	await setMainLed({r:0,g:0,b:255})
 }
- 
+
